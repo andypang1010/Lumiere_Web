09fd5db2ff89104b20bc26561a85694c8b7b55d3
PORRIDGE-ZOU <porridgeorge@gmail.com>
2022-05-08 18:09:58 -0400

    First exploration of movie Item

--- lumiere/pages/_app.tsx
+++ lumiere/pages/_app.tsx
@@ -1,11 +1,3 @@
-// import '../styles/globals.css'
-// import type { AppProps } from 'next/app'
-
-// function MyApp({ Component, pageProps }: AppProps) {
-//   return <Component {...pageProps} />
-// }
-
-// export default MyApp
 import { ChakraProvider } from "@chakra-ui/react"
 import { AppProps } from "next/app"
 
