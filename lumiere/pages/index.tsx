6e35f6946df9881f8b5e87802eac692f6ab0a339
PORRIDGE-ZOU <porridgeorge@gmail.com>
2022-05-08 21:39:33 -0400

    Update index.tsx

--- lumiere/pages/index.tsx
+++ lumiere/pages/index.tsx
@@ -1,5 +1,6 @@
 import { Heading } from "@chakra-ui/react"
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, onSnapshot, QuerySnapshot } from "firebase/firestore";
+import { useEffect, useState } from "react";
 import Layout from "../components/layout/Layout"
 import TextWithName from "../components/layout/TextWithName"
 import TwoColumns from "../components/layout/TwoColumns"
@@ -7,8 +8,18 @@ import { db } from "../util/firebase";
 
 // const userDocumentRef = doc(db, "Users", "kingbob1");
 // const userSnapshot = await getDoc(userDocumentRef);
+const queryName=query();//...
 
 function IndexPage() {
+const [userName, setUserName]=useState<String>('');
+useEffect(()=>{
+  const name=onSnapshot(queryName,(querySnapshot)=>{
+    const username="";//...
+    setUserName(username);
+  })
+
+})
+
   return (
     <Layout title="Lumiere">
       <Heading my='4' size='4xl' textAlign='center' textColor="#495997">
