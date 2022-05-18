c755a65a678cea2bd4bc048ebd7e6986aaf33b45
PORRIDGE-ZOU <porridgeorge@gmail.com>
2022-05-18 16:46:06 -0400

    Update GreetUser.tsx

--- lumiere/components/layout/GreetUser.tsx
+++ lumiere/components/layout/GreetUser.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import { Text } from "@chakra-ui/react"
 
 type Props = {
-    readonly username: string
+    readonly username: String
   }
 
 const GreetUser = ({ username } : Props) => (
